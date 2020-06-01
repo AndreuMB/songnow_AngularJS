a02ebd661393109080b015f5a5a827baaf7a53cb
AndreuMB <andreuolleria@gmail.com>
2020-06-01 11:07:35 +0200

    filters
    module songs filters with checkboxs

--- frontend/modules/songs/controller/songs.controller.js
+++ frontend/modules/songs/controller/songs.controller.js
@@ -1,9 +1,26 @@
+function filter(songs,checks,fsongs){
+    for(var i=0;i<songs.length;i++){
+        for(var x=0;x<checks.length;x++){
+            if(songs[i].playlists===checks[x].id){
+                fsongs.push(songs[i]);
+            }
+        }
+    }
+    if(fsongs.length==0){
+        fsongs=songs;
+    }
+    // console.log(fsongs);
+    return fsongs;
+}
+
 songnow.controller('songsCtrl', function($scope,services,toastr,songs,categories){
     
 console.log(songs);
 console.log(categories);
 $scope.categories=categories;
 $scope.songs=songs;
+var fsongs = [];
+
 var modal = document.getElementById("myModal");
 
 $scope.modal=function(id){
@@ -15,58 +32,56 @@ $scope.modal=function(id){
     var modal = document.getElementById("myModal");
 
     modal.style.display = "block";
-
-    // $scope.showModal = true;
     }
-    // songs.indexOf(id);
+
 }
 var span = document.getElementsByClassName("close")[0];
 span.onclick = function() {
     modal.style.display = "none";
-  }
+}
 
-// for(var i=0;i<categories.length;i++){
-//     console.log(categories[i].id);
-//     checkBox = document.getElementById("categories[i].id");
-//     if (checkBox.checked == true){
-//         categoriesm =  songs.filter(function(category) {
-//             songst=songs.playlists == checkBox;
-//         });
-//       }
-// }
 
-  
-// document.getElementById("categories[i].id");
-// console.log(songst);
-// window.onload = function() {
-//         //     for(var i=0;i<categories.length;i++){
-//         //     console.log(categories[i].id);
-//         //     checkBox = document.getElementById(categories[i].id);
-//         //     checkBox.onchange = function() {
-//         //         console.log(checkBox)
-//         //     }
-//         //     // if (checkBox.checked == true){
-//         //     //     console.log("checkbox")
-//         //     //     // categoriesm =  songs.filter(function(category) {
-//         //     //         // songst=songs.playlists == checkBox;
-//         //     //     // });
-//         //     // }
-//         // }
-//         for(var i=0;i<categories.length;i++){
-//             checkBox = document.getElementById(categories[i].id);
-//             checkBox.onchange = function() {
-//             if (checkBox.checked == true){
-//                 console.log("checked")
-//             }
-//             }
-//         }
+window.onload = function() {
+    var checkBox=[];
+    var checks=[];
+    var chk="";
 
-// }
-//             checkBox = document.getElementById("14");
-//             checkBox.onchange = function() {
-//                 console.log("checkbox")
-// };
-// }
+    for(var i=0;i<categories.length;i++){
+        checkBox[i]=document.getElementById(categories[i].id);
+    }
+    
+    for(var i=0;i<categories.length;i++){
+        console.log(checkBox[i]);
+        checkBox[i].onclick= function click(id) {
+            // console.log(id.path[0].attributes[1].nodeValue);
+            fsongs=[];
+            var idc=id.path[0].attributes[1].nodeValue;
+            chk=document.getElementById(idc);
+            if (chk.checked == true){
+
+                var found = categories.find(function(element) { 
+                return element.id === idc;
+                }); 
+
+                checks.push(found);
+                $scope.songs=filter(songs,checks,fsongs);
+                $scope.$apply();
+
+            }else{
+                for(var i=0;i<checks.length;i++){
+                    if(checks[i].id===idc){
+                        console.log("enter");
+                        checks.splice(i, 1);
+                        $scope.songs=filter(songs,checks,fsongs);
+                        $scope.$apply();
+                    }
+                }
+            }
+            console.log(checks);
+        }
+    }
+}
+
 
 
 });
